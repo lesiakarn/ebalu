26d29607ba3db4b88c868eeae5744bd7c84184a8
lesiakarn <lesiakarn@gmail.com>
2025-01-11 01:08:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 import asyncpg
