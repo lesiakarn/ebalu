2fc9227d277d1973260ea27a8d046653afeff7d3
lesiakarn <lesiakarn@gmail.com>
2025-01-12 20:48:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,13 +100,16 @@ async def update_user_balance(user_id, amount):
 
 async def log_action(action, user_id, username, details=""):
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"[{current_time}] ACTION: {action}, USER_ID: {user_id}, USER_NAME {username}, DETAILS: {details}"
+    log_message = f"[{current_time}] ACTION: {action}, USER_ID: {user_id}, USER_NAME: {username}, DETAILS: {details}"
     print(log_message)
     # Надсилаємо лог адміністратору
-    await bot.send_message(ADMIN_ID, f"📋 Лог дій:\n{log_message}")
+    admins = await get_admins()  # Отримуємо список адміністраторів
     if action == "buy":
         for admin in admins:
-            await bot.send_message(admin["user_id"], f"🛒 Користувач {username} здійснив покупку: {details}")
+            await bot.send_message(admin["user_id"], f"🛒 Користувач @{username} здійснив покупку: {details}")
+    else:
+        awaisend_message(ADMIN_ID, f"📋 Лог дій:\n{log_message}")t bot.
+
 
 # Хендлери
 @dp.message(Command("start"))
